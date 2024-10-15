e276c76db777c1702a05195be198f56d787cad20
Adir Noyman <ofahol2007@hotmail.com>
2024-10-15 23:05:40 +0300

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express, {Request, Response} from "express"
+import cors from "cors"
+import "dotenv/config"
+
+const app = express()
+
+
+// MIDDLEWARE
+app.use(express.json())
+app.use(cors())
+
+app.get("/", async (req: Request, res: Response) => {
+
+    res.json({message: "Hello Adiros 🤓"})
+
+})
+
+app.listen(7001, () => {
+
+    console.log("Server started on localhost:7000 😎🤘")
+})

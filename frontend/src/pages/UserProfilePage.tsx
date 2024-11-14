c24ef4b249bfad6fb3e9db79b89ad70b91246bb2
Adir Noyman <ofahol2007@hotmail.com>
2024-11-14 21:22:16 +0200

    .

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,13 +1,13 @@
 import { useGetUser, useUpdateUserProfile } from '@/api/UserApi';
 import UserProfileForm from '@/forms/user-profile-form/UserProfileForm';
-import { spawn } from 'child_process';
 
 const UserProfilePage = () => {
+  // The  useGetUser() function will run as soon as the UserProfilePage loads and will store it in the currentUser variable
   const {currentUser, isLoading: isGetLoading} = useGetUser()
   const { updateUser, isLoading: isUpdateLoading } = useUpdateUserProfile();
 
   if (isGetLoading) {
-
+    // Loading current user data
     return <span>Loading...</span>
   }
 
